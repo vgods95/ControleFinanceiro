1b12c2898780cd690117c07be4b76b26e11f2460
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2022-01-14 14:49:25 -0300

    Cria a API, a BLL e o DAL.

---
+++ ControleFinanceiro.API/ControleFinanceiro.API/Program.cs
@@ -0,0 +1,47 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors();
+builder.Services.AddSpaStaticFiles(diretorio =>
+{
+    diretorio.RootPath = "ControleFinanceiro-UI";
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(op =>
+    {
+        op.JsonSerializerOptions.IgnoreNullValues = true;
+    })
+    .AddNewtonsoftJson(op =>
+    {
+        op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(op => op.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+app.MapControllers();
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = Path.Combine(Directory.GetCurrentDirectory(), "ControleFinanceiro-UI");
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer($"http://localhost:4200/");
+    }
+});
+app.Run();
---
+++ ControleFinanceiro.BLL/Models/Tipo.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleFinanceiro.BLL.Models
+{
+    public class Tipo
+    {
+        public int TipoId { get; set; }
+        public string Nome { get; set; }
+        public virtual ICollection<Categoria> Categorias { get; set; }
+    }
+}
